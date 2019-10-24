bf1110c0aa631d893e7917493d90174240829076
Yoana <marinova.m.y@gmail.com>
2019-10-24 13:59:27 +0300

    ex.5 - ex.6

---
+++ Excercises/Fetching Resultsets with ADO.NET/Introduction to DB Apps/05.ChangeTownNamesCasing/StartUp.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace _05.ChangeTownNamesCasing
+{
+    public class StartUp
+    {
+        //Write a program that changes all town names to uppercase for a given country.
+
+        private static SqlConnection minionsDbConnection = new SqlConnection("Server = LENOVO\\SQLEXPRESS; Database = MinionsDb; Integrated Security = true");
+
+        public static void Main()
+        {
+            try
+            {
+                minionsDbConnection.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Cannot connect to current database.");
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            Console.Write("Country name: ");
+            string countryName = Console.ReadLine();
+
+            using (minionsDbConnection)
+            {
+                SqlCommand changeTownNamesCmd = new SqlCommand(@$"UPDATE Towns
+                                                                    SET Name = UPPER(Name)
+                                                                  WHERE CountryCode = (SELECT c.Id FROM Countries AS c WHERE c.Name = '{countryName}')",
+                                                                  minionsDbConnection);
+
+                using (changeTownNamesCmd)
+                {
+                    int rowsAffected = changeTownNamesCmd.ExecuteNonQuery();
+
+                    if (rowsAffected == 0)
+                    {
+                        Console.WriteLine("No town names were affected.");
+                        return;
+                    }
+
+                    Console.WriteLine($"{rowsAffected} town names were affected.");
+                }
+
+                SqlCommand townNamesAffectedCmd = new SqlCommand(@$"SELECT t.Name 
+                                                                     FROM Towns as t
+                                                                     JOIN Countries AS c ON c.Id = t.CountryCode
+                                                                    WHERE c.Name = '{countryName}'",
+                                                                    minionsDbConnection);
+
+                var reader = townNamesAffectedCmd.ExecuteReader();
+                List<string> townNames = new List<string>();
+
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        string currentTownName = (string)reader["Name"];
+
+                        townNames.Add(currentTownName);
+                    }
+                }
+
+                Console.WriteLine("[" + String.Join(", ", townNames) + "]");
+            }
+        }
+    }
+}
---
+++ Excercises/Fetching Resultsets with ADO.NET/Introduction to DB Apps/06.RemoveVillain/StartUp.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace _06.RemoveVillain
+{
+    public class StartUp
+    {
+        //Write a program that receives the ID of a villain, deletes him from the database and releases his minions from
+        //serving to him.
+        //Make sure all operations go as planned,
+        //otherwise do not make any changes in the database.
+
+        private static SqlConnection minionsDbConnection = new SqlConnection("Server = LENOVO\\SQLEXPRESS; Database = MinionsDB; Integrated Security = true");
+        private static SqlTransaction sqlTransaction;
+
+        public static void Main()
+        {
+            try
+            {
+                minionsDbConnection.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Cannot connect to current database.");
+                Console.WriteLine(e.Message);
+
+                return;
+            }
+
+            int villainId = int.Parse(Console.ReadLine());
+
+            using (minionsDbConnection)
+            {
+                sqlTransaction = minionsDbConnection.BeginTransaction();
+
+                SqlCommand command = minionsDbConnection.CreateCommand();
+                command.Transaction = sqlTransaction;
+
+                try
+                {
+                    command.CommandText = $"SELECT Name FROM Villains WHERE Id = {villainId}";
+
+                    if (command.ExecuteScalar() == null)
+                    {
+                        Console.WriteLine("No such villain was found.");
+                        return;
+                    }
+
+                    string villainName = (string)command.ExecuteScalar();
+
+                    command.CommandText = $"DELETE FROM MinionsVillains WHERE VillainId = {villainId}";
+                    int countOfMinnionsAffected = command.ExecuteNonQuery();
+
+                    command.CommandText = $"DELETE FROM Villains WHERE Id = {villainId}";
+                    command.ExecuteNonQuery();
+
+                    sqlTransaction.Commit();
+
+                    Console.WriteLine($"{villainName} was deleted.");
+                    Console.WriteLine($"{countOfMinnionsAffected} minions were released.");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+
+                    try
+                    {
+                        sqlTransaction.Rollback();
+                    }
+                    catch (Exception exRollback)
+                    {
+                        Console.WriteLine(exRollback.Message);
+                    }
+                }
+            }           
+        }
+    }
+}
